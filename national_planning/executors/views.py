a4cbba3ebe6cc39e4e107b6ca9f3ede30d2d9e39
andres <andresvz07@hotmail.com>
2016-06-09 00:52:21 -0500

    import filters

--- national_planning/executors/views.py
+++ national_planning/executors/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import viewsets, mixins
+from rest_framework import filters
 
 from .serializers import ExecutorSerializer
 from .models import Executor
